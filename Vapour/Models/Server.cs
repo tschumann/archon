27e602c98d8716d518686a4d83924e8abf4f677d
tschumann <schumann.tom@gmail.com>
2025-07-07 17:29:42 +1000

    Added Vapour server.

---
+++ Vapour/Models/Server.cs
@@ -0,0 +1,8 @@
+namespace Models;
+
+class Server
+{
+    public string Name { get; set; }
+
+    public string Address { get; set; }
+}
---
+++ Vapour/Vapour.cs
@@ -0,0 +1,20 @@
+﻿class Vapour
+{
+    public static void Main()
+    {
+        var app = WebApplication.Create();
+
+        var gameServersService = app.MapGroup("/IGameServersService");
+
+        gameServersService.MapGet("/GetServerList/v1/", () =>
+        {
+            return new Models.Server()
+            {
+                Name = "Server",
+                Address = "127.0.0.1:27015"
+            };
+        });
+
+        app.Run();
+    }
+}
