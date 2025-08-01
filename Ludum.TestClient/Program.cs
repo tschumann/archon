96770fbf34bfe5229fcbe104302d3f3b0604f738
tschumann <schumann.tom@gmail.com>
2025-08-01 18:58:19 +1000

    Added test client for Ludum server.

---
+++ Ludum.TestClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+int remotePort = 27015;
+
+if (args.Length > 0)
+{
+    remotePort = Int32.Parse(args[0]);
+
+    Console.WriteLine("Connecting to port {0}", remotePort);
+}
+
+IPAddress remoteServerAddr = IPAddress.Parse("127.0.0.1");
+IPEndPoint remoteEndPoint = new IPEndPoint(remoteServerAddr, remotePort);
+
+UdpClient udpClient = new UdpClient();
+udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+udpClient.Client.Bind(new IPEndPoint(IPAddress.Any, 11000));
+
+// send a PlayerInfo request and get the response
+udpClient.Send([0xFF, 0xFF, 0xFF, 0xFF, 0x55, 0xFF, 0xFF, 0xFF, 0xFF], remoteEndPoint);
+byte[] playerInfoResponse = udpClient.Receive(ref remoteEndPoint);
+
+Console.WriteLine(Encoding.ASCII.GetString(playerInfoResponse));
--- Ludum/Program.cs
+++ Ludum/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Ludum;
 
-class Ludum
+class Program
 {
     private static void StartListener()
     {
