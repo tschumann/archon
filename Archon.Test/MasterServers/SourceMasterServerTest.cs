9b5422786634a35db2e70189e7a3f8f8eb7c3401
tschumann <schumann.tom@gmail.com>
2025-08-08 17:19:09 +1000

    Added Archon master server project.

---
+++ Archon.Test/MasterServers/SourceMasterServerTest.cs
@@ -0,0 +1,34 @@
+﻿using Archon.MasterServers;
+
+namespace Archon.Test.MasterServers
+{
+    [TestClass]
+    public sealed class SourceMasterServerTest
+    {
+        [TestMethod]
+        public void TestIsServerQuery()
+        {
+            Assert.IsFalse(SourceMasterServer.IsServerQuery([]));
+            Assert.IsFalse(SourceMasterServer.IsServerQuery([0xFF]));
+            Assert.IsFalse(SourceMasterServer.IsServerQuery([0x31, 0xFF]));
+            Assert.IsTrue(SourceMasterServer.IsServerQuery([0x31, 0xFF, 0x30, 0x2E, 0x30, 0x2E, 0x30, 0x2E, 0x30, 0x3A, 0x30, 0x00, 0x00]));
+            Assert.IsTrue(SourceMasterServer.IsServerQuery([0x31, 0xFF, 0x31, 0x2E, 0x31, 0x2E, 0x31, 0x2E, 0x31, 0x3A, 0x30, 0x68, 0x87, 0x00]));
+        }
+
+        [TestMethod]
+        public void TestIsValidRegionCode()
+        {
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x00));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x01));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x02));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x03));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x04));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x05));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x06));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0x07));
+            Assert.IsTrue(SourceMasterServer.IsValidRegionCode(0xFF));
+            Assert.IsFalse(SourceMasterServer.IsValidRegionCode(0x08));
+            Assert.IsFalse(SourceMasterServer.IsValidRegionCode(0x10));
+        }
+    }
+}
---
+++ Archon/MasterServers/SourceMasterServer.cs
@@ -0,0 +1,26 @@
+﻿namespace Archon.MasterServers;
+
+public class SourceMasterServer
+{
+    public static byte[]? HandleRequest(byte[] request)
+    {
+        if (IsServerQuery(request))
+        {
+            return [0xFF, 0xFF, 0xFF, 0xFF, 0x66, 0x0A, 0x01, 0x02, 0x06, 0x08, 0x69, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        }
+
+        return null;
+    }
+
+    public static bool IsServerQuery(byte[] request)
+    {
+        // TODO: check the port and filter stuff too
+        return request != null && request.Length >= 13 && request[0] == 0x31 && IsValidRegionCode(request[1]);
+    }
+
+    public static bool IsValidRegionCode(byte regionCode)
+    {
+        return regionCode == 0x00 || regionCode == 0x01 || regionCode == 0x02 || regionCode == 0x03 || regionCode == 0x04 || regionCode == 0x05 || regionCode == 0x06 || regionCode == 0x07 || regionCode == 0xFF;
+    }
+}
+
