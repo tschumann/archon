1cdfa9938954c297b3726c7989923929ce3cbedf
tschumann <schumann.tom@gmail.com>
2025-08-28 21:10:29 +1000

    Fixed warnings by making Server fields required.

--- Vapour/Models/Server.cs
+++ Vapour/Models/Server.cs
@@ -2,7 +2,7 @@ namespace Models;
 
 class Server
 {
-    public string Name { get; set; }
+    public required string Name { get; set; }
 
-    public string Address { get; set; }
+    public required string Address { get; set; }
 }
