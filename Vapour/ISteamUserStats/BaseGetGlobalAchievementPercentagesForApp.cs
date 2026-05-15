d9d4dc49bcd96ee2999e72dca2aa74c1abee5621
tschumann <schumann.tom@gmail.com>
2026-05-15 17:01:10 +1000

    Moved shared logic for /ISteamUserStats/GetGlobalAchievementPercentagesForApp into shared base class.

---
+++ Vapour/ISteamUserStats/BaseGetGlobalAchievementPercentagesForApp.cs
@@ -0,0 +1,50 @@
+﻿using Vapour.Models;
+
+namespace Vapour.ISteamUserStats;
+
+public abstract class BaseGetGlobalAchievementPercentagesForApp
+{
+    public static string MissingGameidErrorMessage = "<html><head><title>Bad Request</title></head><body><h1>Bad Request</h1>Required parameter 'gameid' is missing</body></html>";
+
+    public static string BadRequestErrorMessage = "<html><head><title>Bad Request</title></head><body><h1>Bad Request</h1>Unknown problem determining WebApi request destination.</body></html>";
+
+    /// <summary>
+    /// Get achievements for a given appid or an empty or failure response. Not ideal but it reduces duplication by handling the
+    /// identical empty and failure responses here and letting the controllers themselves handle the different non-empty responses.
+    /// </summary>
+    /// <param name="gameid"></param>
+    /// <returns></returns>
+    protected static (List<Achievement>? achievements, IResult? response) GetAchievementsOrResponse(string? gameid)
+    {
+        // if the gameid parameter is missing altogether, it's an error
+        if (gameid == null)
+        {
+            return (null, Results.Content(MissingGameidErrorMessage, "text/html", statusCode: StatusCodes.Status400BadRequest));
+        }
+
+        int appid = 0;
+
+        if (!Apps.IsValidAppId(gameid, out appid))
+        {
+            return (null, Results.Content(BadRequestErrorMessage, "text/html", statusCode: StatusCodes.Status400BadRequest));
+        }
+
+        if (!Apps.apps.ContainsKey(appid))
+        {
+            // the Dictionary's types depend on what API version is in use but it doesn't actually matter here as it's empty
+            return (null, Results.Ok(new Dictionary<dynamic, dynamic>()));
+        }
+
+        List<Achievement>? achievements = Apps.apps[appid].achievements;
+
+        if (achievements == null)
+        {
+            // the Dictionary's types depend on what API version is in use but it doesn't actually matter here as it's empty
+            return (null, Results.Ok(new Dictionary<dynamic, dynamic>()));
+        }
+        else
+        {
+            return (achievements, null);
+        }
+    }
+}
--- Vapour/ISteamUserStats/GetGlobalAchievementPercentagesForAppV1.cs
+++ Vapour/ISteamUserStats/GetGlobalAchievementPercentagesForAppV1.cs
@@ -5,54 +5,34 @@ namespace Vapour.ISteamUserStats;
 /// <summary>
 /// See https://api.steampowered.com/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v1/?gameid=220
 /// </summary>
-public class GetGlobalAchievementPercentagesForAppV1
+public class GetGlobalAchievementPercentagesForAppV1 : BaseGetGlobalAchievementPercentagesForApp
 {
-    public static string MissingGameidErrorMessage = "<html><head><title>Bad Request</title></head><body><h1>Bad Request</h1>Required parameter 'gameid' is missing</body></html>";
-
-    public static string BadRequestErrorMessage = "<html><head><title>Bad Request</title></head><body><h1>Bad Request</h1>Unknown problem determining WebApi request destination.</body></html>";
-
     public readonly static Delegate Handler = (HttpContext httpContext, string? gameid) =>
     {
-        // if the gameid parameter is missing altogether, it's an error
-        if (gameid == null)
-        {
-            return Results.Content(MissingGameidErrorMessage, "text/html", statusCode: StatusCodes.Status400BadRequest);
-        }
-
-        int appid = 0;
-
-        if (!Apps.IsValidAppId(gameid, out appid))
-        {
-            return Results.Content(BadRequestErrorMessage, "text/html", statusCode: StatusCodes.Status400BadRequest);
-        }
-
-        if (!Apps.apps.ContainsKey(appid))
-        {
-            return Results.Ok(new Dictionary<string, Dictionary<string, Dictionary<string, List<Achievement>>>>());
-        }
-
-        List<Achievement>? achievements = Apps.apps[appid].achievements;
+        (List<Achievement>? achievements, IResult? response) = GetAchievementsOrResponse(gameid);
 
         if (achievements == null)
         {
-            return Results.Ok(new Dictionary<string, Dictionary<string, Dictionary<string, List<Achievement>>>>());
+            return response;
         }
-
-        return Results.Ok(new Dictionary<string, Dictionary<string, Dictionary<string, List<Achievement>>>>
+        else
         {
+            return Results.Ok(new Dictionary<string, Dictionary<string, Dictionary<string, List<Achievement>>>>
             {
-                "achievementpercentages", new Dictionary<string, Dictionary<string,  List<Achievement>>>
                 {
+                    "achievementpercentages", new Dictionary<string, Dictionary<string,  List<Achievement>>>
                     {
-                        "achievements", new Dictionary<string, List<Achievement>>
                         {
+                            "achievements", new Dictionary<string, List<Achievement>>
                             {
-                                "achievement", achievements
+                                {
+                                    "achievement", achievements
+                                }
                             }
                         }
                     }
                 }
-            }
-        });
+            });
+        }
     };
 }
